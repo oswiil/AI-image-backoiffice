93727caabb858b5cdc430cf9b02d6b88c020e7e4
Oswil <jfosanchez@gmail.com>
2023-10-09 23:08:01 +0200

    more changes

--- config/database.js
+++ config/database.js
@@ -9,9 +9,9 @@ module.exports = ({ env }) => {
         connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        database: env('MARIADB_DATABASE', 'strapi'),
+        user: env('MARIADB_USER', 'strapi'),
+        password: env('MARIADB_PASSWORD', 'strapi'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
